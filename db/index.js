f2c318864f1301fd18cb52b4ec7b68ae3915cd23
Pierrot <pierrot.slider@gmail.com>
2021-05-27 10:01:34 +0200

    Relation

--- db/index.js
+++ db/index.js
@@ -4,4 +4,14 @@ const { Comment } = require('./model/Comment');
 const { Tag } = require('./model/Tag');
 const { sequelize } = require('./sequelize');
 
+User.hasMany(Comment);
+Comment.belongsTo(User);
+User.hasMany(Post);
+Post.belongsTo(User);
+Post.hasMany(Comment);
+Comment.belongsTo(Post);
+
+Post.belongsToMany(Tag, { through: 'PostTag'});
+Tag.belongsToMany(Post, { through: 'PostTag'});
+
 module.exports = { Post, User, Comment, Tag, sequelize };
